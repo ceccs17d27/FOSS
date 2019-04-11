58791cd35238ed50f7f7e0b31adf0e43b81f2bec
ceccs17d27 <43027228+ceccs17d27@users.noreply.github.com>
2019-04-11 10:21:19 +0530

    Add files via upload

---
+++ perl1.ceccs17d27.11.04.19.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Hello This Is Jathin\n";
---
+++ perl2.ceccs17d27.11.04.19.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Enter a and b: \n";
+$a=<>;
+$b=<>;
+my $sum=$a+$b;
+my $difference=$a-$b;
+print "sum=$sum\n";
+print "Difference=$difference\n";
